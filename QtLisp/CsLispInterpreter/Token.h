69731d8b7fb5d763c5116dff106f679adc0fe337
Michael Neuroth <michael.neuroth@freenet.de>
2017-04-07 22:50:32 +0200

    Start to port to C++/Qt

---
+++ QtLisp/CsLispInterpreter/Token.h
@@ -0,0 +1,251 @@
+﻿/*
+ * FUEL(isp) is a fast usable embeddable lisp interpreter.
+ *
+ * Copyright (c) 2016 Michael Neuroth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * */
+
+using System;
+using System.Globalization;
+
+namespace CsLisp
+{
+    /// <summary>
+    /// Type for lisp tokens.
+    /// </summary>
+    public enum LispTokenType
+    {
+        ListStart = 0,
+        ListEnd = 1,
+        Symbol = 2,
+        String = 3,
+        Int = 4,
+        Double = 5,
+        Quote = 6,
+        QuasiQuote = 7,
+        UnQuote = 8,
+        UnQuoteSplicing = 9,
+        True = 10,
+        False = 11,
+        Comment = 12,
+        Nil = 13,
+    }
+
+    /// <summary>
+    /// Interface for a lisp token.
+    /// </summary>
+    /// <remark>
+    /// This interface is intended to remove the dependency to the LispToken class in the future.
+    /// </remark>
+    public interface ILispTokenInterface
+    {
+        /// <summary>
+        /// Gets the type of the token.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        LispTokenType Type { get; }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        object Value { get; }
+
+        /// <summary>
+        /// Gets the start position of the token.
+        /// </summary>
+        /// <value>
+        /// The start position.
+        /// </value>
+        int StartPos { get; }
+    }
+
+    /// <summary>
+    /// Lisp token.
+    /// </summary>
+    public class LispToken : ILispTokenInterface
+    {
+        #region constants
+
+        public const string StringStart = "\"";
+        public const string Quote = "'";
+        public const string Quasiquote = "`";
+        public const string Unquote = ",";
+        public const string Unquotesplicing = ",@";
+        public const string Nil = "NIL";
+
+        #endregion
+
+        #region properties
+
+        /// <summary>
+        /// Gets the type of the token.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
+        public LispTokenType Type { get; private set; }
+
+        /// <summary>
+        /// Gets the value.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public object Value { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the start position of the token.
+        /// </summary>
+        /// <value>
+        /// The start position.
+        /// </value>
+        public int StartPos { get; set; }
+
+        /// <summary>
+        /// Gets or sets the stop position of the token.
+        /// </summary>
+        /// <value>
+        /// The stop position.
+        /// </value>
+        public int StopPos { get; set; }
+
+        /// <summary>
+        /// Gets or sets the line no of the token.
+        /// </summary>
+        /// <value>
+        /// The line no.
+        /// </value>
+        public int LineNo { get; set; }
+
+        #endregion
+
+        #region constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LispToken"/> class.
+        /// </summary>
+        /// <param name="text">The text of the token.</param>
+        /// <param name="start">The start position.</param>
+        /// <param name="stop">The stop position.</param>
+        /// <param name="lineNo">The line no.</param>
+        public LispToken(string text, int start, int stop, int lineNo)
+        {
+            int intValue;
+            double doubleValue;
+
+            StartPos = start;
+            StopPos = stop;
+            LineNo = lineNo;
+            Value = text;
+
+            if (text.StartsWith(StringStart))
+            {
+                Type = LispTokenType.String;
+                Value = text.Substring(1, text.Length - 2);
+            }
+            else if (text == Quote)
+            {
+                Type = LispTokenType.Quote;
+            }
+            else if (text == Quasiquote)
+            {
+                Type = LispTokenType.QuasiQuote;
+            }
+            else if (text == Unquote)
+            {
+                Type = LispTokenType.UnQuote;
+            }
+            else if (text == Unquotesplicing)
+            {
+                Type = LispTokenType.UnQuoteSplicing;
+            }
+            else if (text == "(")
+            {
+                Type = LispTokenType.ListStart;
+            }
+            else if (text == ")")
+            {
+                Type = LispTokenType.ListEnd;
+            }
+            else if (Int32.TryParse(text, out intValue))
+            {
+                Type = LispTokenType.Int;
+                Value = intValue;
+            }
+            else if (Double.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out doubleValue))
+            {
+                Type = LispTokenType.Double;
+                Value = doubleValue;
+            }
+            else if (text.Equals("true") || text.Equals("#t"))
+            {
+                Type = LispTokenType.True;
+                Value = true;
+            }
+            else if (text.Equals("false") || text.Equals("#f"))
+            {
+                Type = LispTokenType.False;
+                Value = false;
+            }
+            else if (text.ToUpper().Equals(Nil))
+            {
+                Type = LispTokenType.Nil;
+                Value = null;
+            }
+            else if (text.StartsWith(";"))
+            {
+                Type = LispTokenType.Comment;
+                Value = text;
+            }
+            else
+            {
+                Type = LispTokenType.Symbol;
+                Value = text;
+            }
+        }
+
+        #endregion
+
+        #region public methods
+
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            if (Type == LispTokenType.Nil)
+            {
+                return Nil;
+            }
+            return Value.ToString();
+        }
+
+        #endregion
+    }
+}
---
+++ QtLisp/CsLispInterpreter/Tokenizer.h
@@ -0,0 +1,238 @@
+﻿/*
+ * FUEL(isp) is a fast usable embeddable lisp interpreter.
+ *
+ * Copyright (c) 2016 Michael Neuroth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR 
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+ * OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * */
+
+using System;
+using System.Collections.Generic;
+
+namespace CsLisp
+{
+    /// <summary>
+    /// The FUEL lisp tokenizer
+    /// </summary>
+    public class LispTokenizer
+    {
+        #region public static methods
+
+        /// <summary>
+        /// Tokenizes the specified code.
+        /// </summary>
+        /// <param name="code">The code.</param>
+        /// <param name="offset">The position offset (decorated code).</param>
+        /// <returns>Container with tokens</returns>
+        public static IEnumerable<LispToken> Tokenize(string code, int offset = 0)
+        {
+            var tokens = new List<LispToken>();
+            var currentToken = String.Empty;
+            var currentTokenStartPos = 0;
+            var lineCount = 1;
+            var isInString = false;
+            var isInSymbol = false;
+            var wasLastBackslash = false;
+
+            Action<string, int, int> addToken = (currentTok, pos, line) =>
+            {
+                tokens.Add(new LispToken(currentTok, currentTokenStartPos - offset, pos - offset, line));
+                isInSymbol = false;
+                isInString = false;
+                currentToken = String.Empty;
+                currentTokenStartPos = pos+1;
+            };
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                char ch = code[i];
+                if (Char.IsWhiteSpace(ch))
+                {
+                    if (isInString)
+                    {
+                        currentToken += ch;
+                    }
+                    else if (isInSymbol)
+                    {
+                        addToken(currentToken, i, lineCount);
+                    }
+                    wasLastBackslash = false;
+                    if (ch == '\n')
+                    {
+                        lineCount++;
+                    }
+                }
+                else if (ch == '\\')
+                {
+                    if (wasLastBackslash)
+                    {
+                        currentToken += ch;
+                        wasLastBackslash = false;
+                    }
+                    else
+                    {
+                        wasLastBackslash = true;
+                    }
+                }
+                else if (ch == '(' || ch == ')')
+                {
+                    if (isInString)
+                    {
+                        currentToken += ch;
+                    }
+                    else if (isInSymbol)
+                    {
+                        addToken(currentToken, i, lineCount);
+                        addToken(String.Empty + ch, i, lineCount);
+                    }
+                    else
+                    {
+                        addToken(String.Empty + ch, i, lineCount);
+                    }
+                    wasLastBackslash = false;
+                }
+                else if (ch == ';')
+                {
+                    if (isInString)
+                    {
+                        currentToken += ch;
+                    }
+                    else if (isInSymbol)
+                    {
+                        addToken(currentToken, i, lineCount);
+                        i = ProcessComment(code, i, lineCount, ch, addToken);
+                    }
+                    else
+                    {
+                        i = ProcessComment(code, i, lineCount, ch, addToken);
+                    }
+                    wasLastBackslash = false;
+                    // comment ends always with new line
+                    lineCount++;
+                }
+                else if (ch == '\'' || ch == '`' || ch == ',')
+                {
+                    if (isInString)
+                    {
+                        currentToken += ch;
+                    }
+                    else
+                    {
+                        if (code[i + 1] == '@')
+                        {
+                            // process unquotesplicing
+                            string s = String.Empty;
+                            s += ch;
+                            i++;
+                            s += code[i];
+                            addToken(s, i, lineCount);
+                        }
+                        else
+                        {
+                            addToken(String.Empty + ch, i, lineCount);
+                        }
+                    }
+                    wasLastBackslash = false;
+                }
+                else if (ch == '"')
+                {
+                    if (wasLastBackslash)
+                    {
+                        currentToken += ch;
+                    }
+                    else if (isInString)
+                    {
+                        // finish string
+                        addToken("\"" + currentToken + "\"", i, lineCount);
+                    }
+                    else
+                    {
+                        // start string
+                        isInString = true;
+                        currentToken = String.Empty;
+                    }
+                    wasLastBackslash = false;
+                }
+                else
+                {
+                    if (!isInSymbol && !isInString)
+                    {
+                        isInSymbol = true;
+                    }
+                    if (wasLastBackslash)
+                    {
+                        ch = ProcessCharAfterBackslash(ch);
+                    }
+                    currentToken += ch;
+                    wasLastBackslash = false;
+                }
+            }
+            if (currentToken != String.Empty)
+            {
+                addToken(currentToken, -1, lineCount);
+            }
+            return tokens;
+        }
+
+        #endregion
+
+        #region private static methods
+
+        private static char ProcessCharAfterBackslash(char ch)
+        {
+            switch (ch)
+            {
+                case 'n':
+                    return '\n';
+                case 'r':
+                    return '\r';
+                case 't':
+                    return '\t';
+                case '\\':
+                    return '\\';
+            }
+            throw new LispException(string.Format("Invalid character after backslash {0}", ch));
+        }
+
+        private static int ProcessComment(string code, int i, int lineCount, char ch, Action<string, int, int> addToken)
+        {
+            int newIndex;
+            var comment = String.Empty + ch + GetRestOfLine(code, i + 1, out newIndex);
+            addToken(comment, i, lineCount);
+            i = newIndex;
+            return i;
+        }
+
+        private static string GetRestOfLine(string code, int i, out int newIndex)
+        {
+            var rest = code.Substring(i);
+            var pos = rest.IndexOf("\n", StringComparison.InvariantCulture);
+            if (pos > 0)
+            {
+                newIndex = i + pos;
+                return rest.Substring(0, pos+1);
+            }
+            newIndex = i + rest.Length -1;
+            return rest;
+        }
+
+        #endregion
+    }
+}
