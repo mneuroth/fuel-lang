14e0d25b0f8ad4f8cb94eaa6b9ae32cc5948024d
Michael Neuroth <michael.neuroth@freenet.de>
2017-11-26 21:14:13 +0100

    Add deployment script

---
+++ deploy_bin.bat
@@ -0,0 +1,19 @@
+ren call with two parameters: OS BUILD_NO VCS_BUILD_NO	
+mkdir deploy
+cd deploy
+mkdir %1
+cd %1
+mkdir %2
+cd %2
+copy ..\..\..\CsLisp\bin\release\fuel.exe
+copy ..\..\..\CsLisp\bin\release\fuel.pdb
+copy ..\..\..\CsLisp\bin\release\fuel.exe.config
+copy ..\..\..\CsLisp\bin\release\FuelInterpreter.dll
+copy ..\..\..\CsLisp\bin\release\FuelInterpreter.pdb
+copy ..\..\..\CsLisp\bin\release\FuelCompiler.dll
+copy ..\..\..\CsLisp\bin\release\FuelCompiler.pdb
+copy ..\..\..\CsLisp\bin\release\FuelDebugger.dll
+copy ..\..\..\CsLisp\bin\release\FuelDebugger.pdb
+mkdir Library
+cd Library
+copy ..\..\..\..\CsLisp\bin\release\Library\fuellib.fuel
