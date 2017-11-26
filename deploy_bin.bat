b26c1dc10648f35096879846d473ec3bb41c50b8
Michael Neuroth <michael.neuroth@freenet.de>
2017-11-26 22:11:57 +0100

    Restore deployment structure

--- deploy_bin.bat
+++ deploy_bin.bat
@@ -1,12 +1,13 @@
-ren call with two parameters: OS APPLICATION_VERSION BUILD_NO 
+ren call with two parameters: OS APPLICATION_VERSION BUILD_NO PROCESSOR_ARCHITECTURE
+rem see: https://stackoverflow.com/questions/20353729/how-can-i-build-a-git-tag-in-teamcity
 mkdir deploy
 cd deploy
 mkdir %1
 cd %1
 mkdir %2
 cd %2
-mkdir %4
-cd %4
+mkdir %3
+cd %3
 copy ..\..\..\..\CsLisp\bin\release\fuel.exe
 copy ..\..\..\..\CsLisp\bin\release\fuel.pdb
 copy ..\..\..\..\CsLisp\bin\release\fuel.exe.config
