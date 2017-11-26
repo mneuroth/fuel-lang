c7f579deedbe0e2aeda5f5b85f3c476d4ef86f12
Michael Neuroth <michael.neuroth@freenet.de>
2017-11-26 21:35:00 +0100

    Added application version to binary deploy script

--- deploy_bin.bat
+++ deploy_bin.bat
@@ -1,19 +1,21 @@
-ren call with two parameters: OS BUILD_NO VCS_BUILD_NO
+ren call with two parameters: OS APPLICATION_VERSION BUILD_NO 
 mkdir deploy
 cd deploy
 mkdir %1
 cd %1
+mkdir %2
+cd %2
 mkdir %3
 cd %3
-copy ..\..\..\CsLisp\bin\release\fuel.exe
-copy ..\..\..\CsLisp\bin\release\fuel.pdb
-copy ..\..\..\CsLisp\bin\release\fuel.exe.config
-copy ..\..\..\CsLisp\bin\release\FuelInterpreter.dll
-copy ..\..\..\CsLisp\bin\release\FuelInterpreter.pdb
-copy ..\..\..\CsLisp\bin\release\FuelCompiler.dll
-copy ..\..\..\CsLisp\bin\release\FuelCompiler.pdb
-copy ..\..\..\CsLisp\bin\release\FuelDebugger.dll
-copy ..\..\..\CsLisp\bin\release\FuelDebugger.pdb
+copy ..\..\..\..\CsLisp\bin\release\fuel.exe
+copy ..\..\..\..\CsLisp\bin\release\fuel.pdb
+copy ..\..\..\..\CsLisp\bin\release\fuel.exe.config
+copy ..\..\..\..\CsLisp\bin\release\FuelInterpreter.dll
+copy ..\..\..\..\CsLisp\bin\release\FuelInterpreter.pdb
+copy ..\..\..\..\CsLisp\bin\release\FuelCompiler.dll
+copy ..\..\..\..\CsLisp\bin\release\FuelCompiler.pdb
+copy ..\..\..\..\CsLisp\bin\release\FuelDebugger.dll
+copy ..\..\..\..\CsLisp\bin\release\FuelDebugger.pdb
 mkdir Library
 cd Library
-copy ..\..\..\..\CsLisp\bin\release\Library\fuellib.fuel
+copy ..\..\..\..\..\CsLisp\bin\release\Library\fuellib.fuel
