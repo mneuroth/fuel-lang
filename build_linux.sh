8f3bf2078f736d9de7c163e1885d3924a4916295
Michael Neuroth <michael.neuroth@freenet.de>
2019-01-21 22:12:55 +0100

    Updated buildscripts for travis ci

--- build_linux.sh
+++ build_linux.sh
@@ -1,20 +1,21 @@
 cd CppLisp
 cd CppLispInterpreter
 rm *.o
-g++ -c -O2 -std=gnu++11 -fexceptions    csobject.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    csstring.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    cstypes.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Debugger.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Environment.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Exception.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Interpreter.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Lisp.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Parser.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Scope.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Token.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Tokenizer.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Utils.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    Variant.cpp
-g++ -c -O2 -std=gnu++11 -fexceptions    fuel.cpp
-g++ fuel.o Variant.o Utils.o Tokenizer.o Token.o Scope.o Parser.o Lisp.o Interpreter.o Exception.o Environment.o Debugger.o cstypes.o csstring.o csobject.o -o fuel -lstdc++ -lm -lgcc
+$CXX -c -O2 -std=gnu++11 -fexceptions    csobject.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    csstring.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    cstypes.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Debugger.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Environment.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Exception.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Interpreter.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Lisp.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Parser.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Scope.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Token.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Tokenizer.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Utils.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    Variant.cpp
+$CXX -c -O2 -std=gnu++11 -fexceptions    fuel.cpp
+$CXX fuel.o Variant.o Utils.o Tokenizer.o Token.o Scope.o Parser.o Lisp.o Interpreter.o Exception.o Environment.o Debugger.o cstypes.o csstring.o csobject.o -o fuel -lstdc++ -lm -lgcc
+ls -lrt
 
