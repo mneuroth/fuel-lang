16449c8b5be6056ed941e1aac8cbd284898dc759
Michael Neuroth <michael.neuroth@freenet.de>
2019-01-22 22:12:56 +0100

    Try to build for macos on travis ci

--- build_linux.sh
+++ build_linux.sh
@@ -2,11 +2,14 @@ cd CppLisp
 cd CppLispInterpreter
 qmake CppLispInterpreter.pro
 make 
+g++ -v
+gcc -v
 ./fuel -v
-ls -l
+./fuel  -e "(println (platform))"
+#ls -l
 cd ..
 cd QtLispInterpreterUnitTests
 qmake QtLispInterpreterUnitTests.pro
 make 
 ./tst_qtlispinterpreterunitteststest
-ls -l
+#ls -l
