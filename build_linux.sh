849e5f653c3b4f925af3c3565db0d0a5a5b5c32e
Michael Neuroth <michael.neuroth@freenet.de>
2019-01-22 22:42:39 +0100

    Try to support builds with clang and gcc

--- build_linux.sh
+++ build_linux.sh
@@ -1,13 +1,21 @@
 cd CppLisp
 cd CppLispInterpreter
-qmake "QMAKE_CXX = clang++" CppLispInterpreter.pro
+if [ "$CXX" == "clang++" ]; then
+    qmake "QMAKE_CXX = clang++" CppLispInterpreter.pro
+else
+    qmake CppLispInterpreter.pro
+fi
 make 
 ./fuel -v
 ./fuel  -e "(println (platform))"
 #ls -l
 cd ..
 cd QtLispInterpreterUnitTests
-qmake QtLispInterpreterUnitTests.pro
+if [ "$CXX" == "clang++" ]; then
+    qmake "QMAKE_CXX = clang++" QtLispInterpreterUnitTests.pro
+else
+    qmake QtLispInterpreterUnitTests.pro
+fi
 make 
 ./tst_qtlispinterpreterunitteststest
 #ls -l
