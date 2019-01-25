11d570607633951f11d19ab2c641855aaffa0bbe
Michael Neuroth <michael.neuroth@freenet.de>
2019-01-25 18:47:36 +0100

    Improved build scripts

--- build_cs.sh
+++ build_cs.sh
@@ -2,11 +2,14 @@ pwd
 ls -lrt
 cd CsLisp
 # msbuild
-msbuild /p:Configuration=Release CsLisp.sln
+MsBuild.exe /p:Configuration=Release CsLisp.sln
 ls -lrt
 cd bin
 Release
 # chmod +x fuel.exe
+ls -lrt
+cd bin
+cd Release
 ls -lrt
 mono fuel.exe -v
 mono fuel.exe -e "(println (platform))"
