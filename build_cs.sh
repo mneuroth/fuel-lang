564ed2a6e98bc694e28d2c9fb5e8368e6879c523
Michael Neuroth <michael.neuroth@freenet.de>
2019-01-25 17:49:49 +0100

    Improve ci build script

---
+++ build_cs.sh
@@ -0,0 +1,11 @@
+pwd
+cd CsLisp
+# msbuild
+msbuild /p:Configuration=Release CsLisp.sln
+ls -lrt
+cd bin
+Release
+# chmod +x fuel.exe
+ls -lrt
+mono fuel.exe -v
+mono fuel.exe -e "(println (platform))"
