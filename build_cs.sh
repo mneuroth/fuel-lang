1c7be1c0269baf3b8425e07f0fa2d44705977419
Michael Neuroth <michael.neuroth@freenet.de>
2019-01-25 19:15:15 +0100

    Updated ci build script

--- build_cs.sh
+++ build_cs.sh
@@ -1,15 +1,16 @@
 pwd
+msbuild
+which msbuild
+which mono
 ls -lrt
 cd CsLisp
 # msbuild
-MsBuild.exe /p:Configuration=Release CsLisp.sln
-ls -lrt
-cd bin
-Release
-# chmod +x fuel.exe
+#MsBuild.exe /p:Configuration=Release CsLisp.sln
+msbuild /p:Configuration=Release CsLisp.sln
 ls -lrt
 cd bin
 cd Release
+# chmod +x fuel.exe
 ls -lrt
 mono fuel.exe -v
 mono fuel.exe -e "(println (platform))"
