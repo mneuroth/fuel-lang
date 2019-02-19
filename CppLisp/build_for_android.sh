8d47d625009d817a57edad72c3cc5d8c2a6b1906
Michael Neuroth <michael.neuroth@freenet.de>
2019-02-19 21:19:50 +0100

    Update build script for android

--- CppLisp/build_for_android.sh
+++ CppLisp/build_for_android.sh
@@ -1,11 +1,11 @@
 # exampe of usage: > ./build_for_android.sh /home/user/programs/android/ndk/android-ndk-r16b armeabi-v7a
 # ABI are: armeabi, armeabi-v7a, arm64-v8a, x86, x86_64
 export ANDROID_NDK_HOME=$1
-export PATH=$PATH:$ANDROID_NDK_HOME
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$2 CMakeLists.txt
+export ANDROID_ABI=$2
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$ANDROID_ABI CMakeLists.txt
 make -j 4
 cp CppLispInterpreter/libFuelInterpreter.* .
 cp CppLispDebugger/libFuelDebugger.* .
 mkdir Library
 cp ../Library/fuellib.fuel Library
-zip -u fuel-lang-android-$2-bin.zip fuel libFuelInterpreter.* libFuelDebugger.* Library/fuellib.fuel      
+zip -u fuel-lang-android-$ANDROID_ABI-bin.zip fuel libFuelInterpreter.* libFuelDebugger.* Library/fuellib.fuel
