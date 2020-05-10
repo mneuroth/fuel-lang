44d7dc6847ec60576485619b8f3e80aac2b12b50
Michael Neuroth <michael.neuroth@freenet.de>
2020-05-10 11:58:03 +0200

    Build release version for android with cmake, added strip operation in cmake for release builds

--- CppLisp/build_for_android.sh
+++ CppLisp/build_for_android.sh
@@ -9,7 +9,7 @@ fi
 ANDROID_NDK_HOME=$1
 ANDROID_ABI=$2
 echo *** Build fuel with shared libraries ***
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$ANDROID_ABI CMakeLists.txt
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$ANDROID_ABI CMakeLists.txt
 make clean
 make -j 4
 cp CppLispInterpreter/libFuelInterpreter.* .
@@ -19,7 +19,7 @@ cp ../Library/fuellib.fuel Library
 zip -u fuel-lang-android-$ANDROID_ABI-bin.zip fuel libFuelInterpreter.* libFuelDebugger.* Library/fuellib.fuel
 echo *** Build fuel without shared libraries ***
 cd CppLisp
-cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$ANDROID_ABI CMakeLists.txt
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake -DANDROID_ABI=$ANDROID_ABI CMakeLists.txt
 make clean
 make -j 4
 mkdir Library
